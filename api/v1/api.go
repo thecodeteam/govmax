dffa7d7a6f6eea5de991450f03e78b474362b104
Kimberly Lu <kimberly.lu@emc.com>
2015-10-14 16:21:50 -0400

    Kim commit

--- api/v1/api.go
+++ api/v1/api.go
@@ -2,36 +2,58 @@ package apiv1
 
 import (
 	"errors"
-	"os"
-	"strconv"
-)
+		"os"
+			"strconv"
+			)
 
-type GetStoragePoolsResp struct {
-	Entries GetStoragePoolsResp_entries `json:"entries"`
-}
+			type GetStoragePoolsResp_entries struct {
+				Entries `json:"entries"`
+					Content []struct {
+									StoragePoolName string `json:"i$ElementName"`
+													InstanceName string `json:"i$InstanceID"`
+														}
+														}
 
-type GetStoragePoolsResp_entries struct {
-	Content GetStoragePoolsResp_entries_content `json:"content"`
-}
+														//Get a list of all Storage Pools associated with SymmID
+														func (smis *SMIS) GetStoragePools(sid string) (resp *GetStoragePoolsResp, err error){
+															err = smis.query("GET","/ecom/edaa/root/emc/instances/Symm_StorageSystem/CreationClassName::SymmStorageSystem,Name::" + sid + "/relationships/CIM_StoragePool", nil, &resp)
+																jaon.UnmRAHl
+																	return resp,err
+																	}
 
-type GetStoragePoolsResp_entries_content struct {
-	StoragePoolName string `json:"i$ElementName"`
-	InstanceName string `json:"i$InstanceID"`
-}
+																	//Get a list of Storage Volumes to/associated with a Device Masking Group
+																	func (smis *SMIS) GetDeviceMaskingGroups(sid string) (resp *GetDeviceMaskingGroupsResp, err error){
+																		err = smis.query("GET","/ecom/edaa/root/emc/instances/Symm_StorageSystem/CreationClassName::Symm_StorageSystem,Name::" + sid + "/relationships/CIM_DeviceMaskingGroup", nil, &resp)
+																			return resp,err
+																			}
 
-func (smis *SMIS) GetStoragePools(sid string) (resp *GetStoragePoolsResp, err error){
-	err = smis.query("GET","/ecom/edaa/root/emc/instances/Symm_StorageSystem/CreationClassName::SymmStorageSystem,Name::" + sid + "/relationships/CIM_StoragePool", nil, &resp)
-	return resp,err
-}
+																			//Get a list of Storage Volumes on/associated with SymmID
+																			func (smis *SMIS) GetStorageVolumes(sid string) (resp *GetStorageVolumesResp, err error){
+																				err = smis.query("GET","/ecom/edaa/root/emc/instances/Symm_StorageSystem/CreationClassName::Symm_StorageSystem,Name::" + sid + "/relationships/CIM_StorageVolume", nil, &resp)
+																					return resp,err
+																					}
 
-type PostVolumesReq struct {
+																					type PostVolumesReq struct {
+																					{
+																						Content PostVolumesContent `json: "content"`
+																						}
 
+																						type PostVolumesContent struct {
+																							ElementType string `json: "ElementType"`
+																								Size string `json: "Size"`
+																									ElementName string `json: "ElementName"`
+																										EMCNumberOfDevices string `json: "EMCNumberOfDevices"`
+																											Type string `json: "@type"`
+																											}
+																											//Add goal struct later 
 
-}
+																											type PostVolumesResp struct {
 
-type PostVolumesResp struct {
+																											}
 
-}
+																											//Create a Storage Volume
+																											func (smis *SMIS) PostVolumes(req *PostVolumesReq, sid string) (resp *PostVolumesResp, err error){
+																												err = smis.query("POST","/ecom/edaa/root/emc/instances/Symm_StorageConfigurationService/CreationClassName::Symm_StorageConfigurationService,Name::EMCStorageConfigurationService,SystemCreationClassName::Symm_StorageSystem,SystemName::" + sid + "/action/CreateOrModifyElementFromStoragePool", req, &resp)
+																													return resp,err
+																													}
 
-func (smis *SMIS) PostVolumes(req *PostVolumesReq, sid string) (resp *PostVolumesResp, err error){
-	err = smis.query("POST","/ecom/edaa/root/emc/instances/Symm_StorageConfigurationService/CreationClassName::Symm_StorageConfigurationService,Name::EMCStorageConfigurationService,SysytemCreateionClassName::Symm_StorageSystem,SystemName::" + sid + "/action/CreateOrModifyElementFromStoragePool/action/CreateOrModifyElementFromStoragePool",

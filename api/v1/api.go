f95b6d3375868fd1d8f510e4a7d2d89f032ea741
Kimberly Lu <kimberly.lu@emc.com>
2015-10-15 20:18:50 -0400

    Added Post Device Masking & Initiator Group and structs

--- api/v1/api.go
+++ api/v1/api.go
@@ -6,23 +6,42 @@ import (
 	"strconv"
 	)
 
-type GetStoragePoolsResp_entries struct {
-	Entries `json:"entries"`
-	Content []struct {
-		StoragePoolName string `json:"i$ElementName"`
-		InstanceName string `json:"i$InstanceID"`
-		}
-	}
+type GetStoragePoolsResp struct {
+	Entries `json:"entries"`{
+		Content []struct {
+			StoragePoolName string `json:"i$ElementName"`
+			InstanceName string `json:"i$InstanceID"`
+		} `json:"content"`
+	} `json:"entries"`
+}
+
 //Get a list of all Storage Pools associated with SymmID
 func (smis *SMIS) GetStoragePools(sid string) (resp *GetStoragePoolsResp, err error){
 	err = smis.query("GET","/ecom/edaa/root/emc/instances/Symm_StorageSystem/CreationClassName::SymmStorageSystem,Name::" + sid + "/relationships/CIM_StoragePool", nil, &resp)
 	return resp,err
 }
 
-//Get a list of Storage Volumes to/associated with a Device Masking Group
+type GetDeviceMaskingGroupsResp struct {
+	Entries `json:"entries"`{
+		Content []struct {
+			DeviceMaskingGroupName string `json:"i$ElementName"`
+			InstanceName string `json:"i$InstanceID"`
+		} `json:"content"`
+	} `json:"entries"`
+}
+
+//Get a list of Device Masking Groups associated with SymmID
 func (smis *SMIS) GetDeviceMaskingGroups(sid string) (resp *GetDeviceMaskingGroupsResp, err error){
 	err = smis.query("GET","/ecom/edaa/root/emc/instances/Symm_StorageSystem/CreationClassName::Symm_StorageSystem,Name::" + sid + "/relationships/CIM_DeviceMaskingGroup", nil, &resp)
 	return resp,err
+}
+
+type GetStorageVolumesResp struct {
+	Entries `json:"entries"`{
+		Content []struct {
+			DeviceName string `json:"i$DeviceID"`
+		} `json:"content"`
+	} `json:"entries"`
 }
 
 //Get a list of Storage Volumes on/associated with SymmID
@@ -31,19 +50,35 @@ func (smis *SMIS) GetStorageVolumes(sid string) (resp *GetStorageVolumesResp, er
 	return resp,err
 }
 
+type PostDeviceMaskingGroupsContent struct {
+	Content []struct {
+		GroupName string `json: "GroupName"`
+		Type int `json: "Type"`	
+		//Type 2 for Initator Group, Type 4 for Device Masking Group
+		@Type string `json: "@type"`
+	} `json:"content"`
+}
+
+//Create a Device Masking Group (Same method call for Create an Initiator Group, but different Type in Content)
+func (smis *SMIS) PostDeviceMaskingGroups (req *PostVolumesReq, sid string) (resp *PostVolumesResp, err error){
+	err = smis.query("POST","/ecom/edaa/root/emc/instances/Symm_ControllerConfigurationService/CreationClassName::Symm_ControllerConfigurationService,Name::EMCControllerConfigurationService,SystemCreationClassName::Symm_StorageSystem,SystemName::" + sid + "/action/CreateGroup", req, &resp)
+	return resp,err
+}
+
 type PostVolumesReq struct {
 
 }
 
 type PostVolumesContent struct {
-
-	ElementType string `json: "ElementType"`
-	Size string `json: "Size"`
-	ElementName string `json: "ElementName"`
-	EMCNumberOfDevices string `json: "EMCNumberOfDevices"`
-	Type string `json: "@type"`
+	Content []struct {
+		ElementType string `json: "ElementType"`
+		Size string `json: "Size"`
+		ElementName string `json: "ElementName"`
+		EMCNumberOfDevices string `json: "EMCNumberOfDevices"`
+		@Type string `json: "@type"`
+		//Add goal stuct later {@type, InstanceID}
+	} `json:"content"`
 }
-//Add goal struct later 
 
 type PostVolumesResp struct {
 
